f0780039adfeddea2845a3340d89dd53dab89026
crazybits <crazybit.github@gmail.com>
2017-12-17 22:58:23 +0800

    add bitcny support aex

--- btsprice/task_exchanges.py
+++ btsprice/task_exchanges.py
@@ -157,6 +157,9 @@ class TaskExchanges(object):
                 "aex_btc", "BTC",
                 self.exchanges.orderbook_aex, "btc", "bts")),
             loop.create_task(self.fetch_orderbook(
+                "aex_bitcny", "BITCNY",
+                self.exchanges.orderbook_aex, "bitcny", "bts")),
+            loop.create_task(self.fetch_orderbook(
                 "zb_btc", "BTC",
                 self.exchanges.orderbook_zb, "btc", "bts")),
             loop.create_task(self.fetch_orderbook(
